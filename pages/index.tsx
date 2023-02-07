2fd75678b8a495c6caa9e8aaa5b85d9c310975eb
CodesBolt <123140179+CodesBolt@users.noreply.github.com>
2023-02-07 18:30:13 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ const Home: NextPage = () => {
   }
   let difficultyObj = {
     'Easy': "Easy",
-    'Professional': "Professional",
+    'Profession': "Profession",
   }
   let text = desc||defultDesc
   
