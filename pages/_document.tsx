74166b99dff7615f264725c2380be0c02ed309a0
igithubcode <icloud.ai@icloud.com>
2023-02-07 18:25:34 +0800

    first

---
+++ pages/_document.tsx
@@ -0,0 +1,44 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+         <link rel="manifest" href="/site.webmanifest"></link>
+          <meta
+            name="description"
+            content="Teach you anything in seconds use AI."
+          />
+          <meta
+            property="og:description"
+            content="Teach you anything in seconds use AI."
+          />
+          <meta property="og:title" content="Teach Anything" />
+          <meta
+            name="twitter:description"
+            content="Teach you anything in seconds use AI."
+          />
+          <meta
+            property="og:image"
+            content="https://futurewei.tech.com/og-image.png"
+          />
+          <meta name="twitter:card" content="summary_large_image"/>
+          <meta name="twitter:site" content="@idingxiaoshan"/>
+          <meta name="twitter:title" content="Teach Anything"/>
+          <meta name="twitter:description" content="Teach you anything in seconds use AI"/>
+          <meta name="twitter:image" content="https://futurewei.tech.com/og-image.png"/>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ pages/index.tsx
@@ -0,0 +1,204 @@
+import { AnimatePresence, motion } from "framer-motion";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import { Toaster, toast } from "react-hot-toast";
+import DropDown, { VibeType } from "../components/DropDown";
+import DropDown2, { VibeType2 } from "../components/DropDown2";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Github from "../components/GitHub";
+import LoadingDots from "../components/LoadingDots";
+import ResizablePanel from "../components/ResizablePanel";
+
+const Home: NextPage = () => {
+  const [loading, setLoading] = useState(false);
+  const [desc, setDesc] = useState("");
+  const [lang, setLang] = useState<VibeType>("English");
+  const [difficulty, setDifficulty] = useState<VibeType2>("Easy");
+  const [generatedDescs, setGeneratedDescs] = useState<string>("");
+  const defultDesc = 'How to explain relativity?'
+
+  console.log("Streamed response: ", {generatedDescs});
+  let promptObj = {
+    'English': "UK English",
+    "中文": "Simplified Chinese",
+    "繁體中文": "Traditional Chinese",
+    "日本語": "Japanese",
+    "Italiano": "Italian",
+    "Deutsch": "German",
+    "Español": "Spanish",
+    "Français": "French",
+    "Nederlands": "Dutch",
+    "한국어": "Korean",
+    "ភាសាខ្មែរ":"Khmer",
+    "हिंदी" : "Hindi"
+  }
+  let difficultyObj = {
+    'Easy': "Easy",
+    'Professional': "Professional",
+  }
+  let text = desc||defultDesc
+  
+  const generateDesc = async (e: any) => {
+    let prompt;
+    if (difficultyObj[difficulty]=="Easy"){
+      prompt = `Explain ${text}${text.slice(-1) === "." ? "" : "."} to a 6nd grader in ${promptObj[lang]} with a simple example.`;
+    } else{
+      prompt = `Explain ${text}${text.slice(-1) === "." ? "" : "."} in ${promptObj[lang]}  in technical terms, divided into two paragraphs, principles and applications. Output format, Principle:, Application.`;
+    }
+    e.preventDefault();
+    setGeneratedDescs("");
+    setLoading(true);
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+      }),
+    });
+    console.log("Edge function returned.");
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    // This data is a ReadableStream
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+    let done = false;
+
+    while (!done) {
+      const { value, done: doneReading } = await reader.read();
+      done = doneReading;
+      const chunkValue = decoder.decode(value);
+      setGeneratedDescs((prev) => prev + chunkValue);
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+      <Head>
+        <title>Teach Anything</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"></link>
+      </Head>
+
+      <Header/>
+
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-2 sm:mt-4">
+        <h1 className="sm:text-3xl text-2xl max-w-1xl font-bold text-slate-900">
+          Teach you anything in seconds
+        </h1>
+        <div className="max-w-xl w-full">
+          <div className="flex mt-4 items-center space-x-3 mb-3">
+            <span className="w-7 h-7 rounded-full bg-black text-white text-center leading-7">1</span>
+            <p className="text-left font-medium">
+              Write your question
+            </p>
+          </div>
+          <textarea
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+            rows={4}
+            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-2"
+            placeholder={
+              "e.g. "+defultDesc
+            }
+          />
+          <div className="flex mb-5 items-center space-x-3">
+            <span className="w-7 h-7 rounded-full bg-black text-white text-center leading-7">2</span>
+            <p className="text-left font-medium">Select your language</p>
+          </div>
+          <div className="block">
+            <DropDown vibe={lang} setVibe={(newLang) => setLang(newLang)} />
+          </div>
+
+          <div className="flex mb-5 items-center space-x-3 mt-3">
+            <span className="w-7 h-7 rounded-full bg-black text-white text-center leading-7">3</span>
+            <p className="text-left font-medium">Select difficulty</p>
+          </div>
+          <div className="block">
+            <DropDown2 vibe2={difficulty} setVibe2={(newDifficulty) => setDifficulty(newDifficulty)} />
+          </div>
+
+          {!loading && (
+            <button
+              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-4 mt-3 hover:bg-black/80 w-full"
+              onClick={(e) => generateDesc(e)}
+            >
+              Get the answer &rarr;
+            </button>
+          )}
+          {loading && (
+            <button
+              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-4 mt-3 hover:bg-black/80 w-full"
+              disabled
+            >
+              <LoadingDots color="white" style="large" />
+            </button>
+          )}
+        </div>
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          toastOptions={{ duration: 2000 }}
+        />
+        <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
+        <ResizablePanel>
+          <AnimatePresence mode="wait">
+            <motion.div className="space-y-10 my-4">
+              {generatedDescs && (
+                <>
+                  <div>
+                    <h2 className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto">
+                      The answer is
+                    </h2>
+                  </div>
+                  <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto  whitespace-pre-wrap">
+
+                    <div
+                      className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border text-left"
+                      onClick={() => {
+                        navigator.clipboard.writeText(generatedDescs);
+                        toast("Text copied to clipboard", {
+                          icon: "✂️",
+                        });
+                      }}
+                    >
+                      <p>{generatedDescs}</p>
+                    </div>
+                  </div>
+                </>
+              )}
+            </motion.div>
+          </AnimatePresence>
+        </ResizablePanel>
+        <div className="mt-2">
+          <a href="https://twitter.com/intent/tweet/?text=Teach%20Anything&url=https%3A%2F%2Fwww.teach-anything.com" target="_blank" className="text-[#1da1f2] font-medium text-sm px-5 py-2.5 text-center inline-flex items-center hover:opacity-80">
+              <svg className="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitter" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M459.4 151.7c.325 4.548 .325 9.097 .325 13.65 0 138.7-105.6 298.6-298.6 298.6-59.45 0-114.7-17.22-161.1-47.11 8.447 .974 16.57 1.299 25.34 1.299 49.06 0 94.21-16.57 130.3-44.83-46.13-.975-84.79-31.19-98.11-72.77 6.498 .974 12.99 1.624 19.82 1.624 9.421 0 18.84-1.3 27.61-3.573-48.08-9.747-84.14-51.98-84.14-102.1v-1.299c13.97 7.797 30.21 12.67 47.43 13.32-28.26-18.84-46.78-51.01-46.78-87.39 0-19.49 5.197-37.36 14.29-52.95 51.65 63.67 129.3 105.3 216.4 109.8-1.624-7.797-2.599-15.92-2.599-24.04 0-57.83 46.78-104.9 104.9-104.9 30.21 0 57.5 12.67 76.67 33.14 23.72-4.548 46.46-13.32 66.6-25.34-7.798 24.37-24.37 44.83-46.13 57.83 21.12-2.273 41.58-8.122 60.43-16.24-14.29 20.79-32.16 39.31-52.63 54.25z"></path></svg>
+              Share on Twitter
+            </a>
+        </div>
+      </main>
+      <Footer />
+      <div className="p-5 text-center md:fixed right-0 bottom-10">
+        <a href="https://www.producthunt.com/posts/teach-anything?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-teach&#0045;anything" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=378102&theme=light" alt="Teach&#0032;Anything - Teach&#0032;you&#0032;anything&#0032;in&#0032;seconds | Product Hunt" width="250" height="54" /></a>
+      </div>            
+    </div>
+  );
+};
+
+export default Home;
